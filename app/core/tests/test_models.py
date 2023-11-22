34bf9cb2d88bbceaec8e79dab7f98b1a20c4b4a2
iameraj <iameraj@outlook.com>
2023-11-22 15:41:29 +0530

    added test for custom models

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,21 @@
+"""
+Tests for models
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+    """Test Models"""
+
+    def test_create_user_with_email(self):
+        """Test that user with email is created properly"""
+        email = 'normal_user@example.com'
+        password = 'iamPassw0rd'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertEqual(user.check_password(password))
+         
