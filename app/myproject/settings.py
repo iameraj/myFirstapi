1ae5caae3d3c4eb1c500e9c46a9b1d53d4649a2b
iameraj <iameraj@outlook.com>
2023-12-05 05:16:51 +0530

    changed database to postgres

--- app/myproject/settings.py
+++ app/myproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,8 +79,12 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME", "devdb"),
+        "USER": os.environ.get("DB_USER", "devuser"),
+        "PASSWORD": os.environ.get("DB_PASS", "jaba jaba"),
+        "HOST": os.environ.get("DB_HOST", "db"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
 
