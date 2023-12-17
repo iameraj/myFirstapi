a5e887e37dc1da5b024b8135c0d7bc4c3cc9497d
iameraj <iameraj@outlook.com>
2023-12-17 18:17:36 +0530

    changed db password

--- app/myproject/settings.py
+++ app/myproject/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("DB_NAME", "devdb"),
         "USER": os.environ.get("DB_USER", "devuser"),
-        "PASSWORD": os.environ.get("DB_PASS", "jaba jaba"),
+        "PASSWORD": os.environ.get("DB_PASS", "jabajaba"),
         "HOST": os.environ.get("DB_HOST", "db"),
         "PORT": os.environ.get("DB_PORT", "5432"),
     }
