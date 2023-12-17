a0bf27e69562823cfe240d78a1a42d01aaa0e1bf
iameraj <iameraj@outlook.com>
2023-12-17 17:14:06 +0530

    changed line length to 79

--- app/myproject/settings.py
+++ app/myproject/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-u=)ddug(17my+9#9a8(=gu0=r%b%m&m*mlki=(k$x9eaaiqfc_"
+SECRET_KEY = (
+    "django-insecure-u=)ddug(17my+9#9a8(=gu0=r%b%m&m*mlki=(k$x9eaaiqfc_"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
