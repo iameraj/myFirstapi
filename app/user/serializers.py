bfdf7410711c5085c59b9ea1642e30ac8abbdedd
iameraj <iameraj@outlook.com>
2024-01-03 19:35:21 +0530

    added token authentication to user model

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -6,7 +6,7 @@ from django.contrib.auth import (
     authenticate,
 )
 from rest_framework import serializers
-from django.utils.translation import gettext as _ 
+from django.utils.translation import gettext as _
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -32,6 +32,7 @@ class AuthTokenSerializer(serializers.Serializer):
     )
 
     def validate(self, attrs):
+        """Validate and authenticate the user."""
         email = attrs.get("email")
         password = attrs.get("password")
         user = authenticate(
