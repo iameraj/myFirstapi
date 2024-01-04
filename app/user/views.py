5ad1746e96a418e5bdc15c397712a8590cb2ea97
iameraj <iameraj@outlook.com>
2024-01-04 18:22:29 +0530

    added update route to api

--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,7 @@
 """
 Views for User API
 """
-from rest_framework import generics
+from rest_framework import generics,authentication,permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from user.serializers import (
@@ -21,3 +21,14 @@ class CreateTokenView(ObtainAuthToken):
 
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """manage and authenticate user"""
+
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """reterive and return athe authenticated user."""
+        return self.request.user
