261e88c5f5c8f739a47a457da28507cd5647f06e
iameraj <iameraj@outlook.com>
2024-02-19 14:22:09 +0530

    added tags model

--- app/core/models.py
+++ app/core/models.py
@@ -58,6 +58,17 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=7, decimal_places=2)
     description = models.TextField(blank=True)
+    tags = models.ManyToManyField('Tags')
 
     def __str__(self):
         return self.title
+
+class Tags(models.Model):
+    """Tag for filtering recipes"""
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(
+            settings.AUTH_USER_MODEL,
+            on_delete=models.CASCADE,
+    )
+    def __str__(self):
+        return self.name
